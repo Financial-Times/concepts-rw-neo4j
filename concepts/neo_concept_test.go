26bdae82b156f4be8d94812e574057ef687de988
MiroslavGatsanoga <miroslav.gatsanoga@ft.com>
2021-09-09 12:18:11 +0300

    Add transform functions tests for simple relations

--- concepts/neo_concept_test.go
+++ concepts/neo_concept_test.go
@@ -197,3 +197,73 @@ func TestToOntologyNewAggregateConcept(t *testing.T) {
 		})
 	}
 }
+
+func TestТоOntologyNewConcept(t *testing.T) {
+	tests := []struct {
+		name            string
+		neoConcept      neoConcept
+		ontologyRelsCfg map[string]ontology.RelationshipConfig
+		expected        ontology.NewConcept
+	}{
+		{
+			name: "simple one-to-one relationship",
+			neoConcept: neoConcept{
+				Types:              []string{"Brand"},
+				ParentOrganisation: "c001ee9c-94c5-11e8-8f42-da24cd01f044",
+			},
+			ontologyRelsCfg: map[string]ontology.RelationshipConfig{
+				"SUB_ORGANISATION_OF": {
+					ConceptField: "parentOrganisation",
+					OneToOne:     true,
+				},
+			},
+			expected: ontology.NewConcept{
+				Type: "Brand",
+				Relationships: []ontology.Relationship{
+					{
+						UUID:  "c001ee9c-94c5-11e8-8f42-da24cd01f044",
+						Label: "SUB_ORGANISATION_OF",
+					},
+				},
+			},
+		},
+		{
+			name: "simple one-to-many relationship",
+			neoConcept: neoConcept{
+				Types:         []string{"Brand"},
+				HasFocusUUIDs: []string{"2e7429bd-7a84-41cb-a619-2c702893e359", "740c604b-8d97-443e-be70-33de6f1d6e67", "c28fa0b4-4245-11e8-842f-0ed5f89f718b"},
+			},
+			ontologyRelsCfg: map[string]ontology.RelationshipConfig{
+				"HAS_FOCUS": {
+					ConceptField: "hasFocusUUIDs",
+				},
+			},
+			expected: ontology.NewConcept{
+				Type: "Brand",
+				Relationships: []ontology.Relationship{
+					{
+						UUID:  "2e7429bd-7a84-41cb-a619-2c702893e359",
+						Label: "HAS_FOCUS",
+					},
+					{
+						UUID:  "740c604b-8d97-443e-be70-33de6f1d6e67",
+						Label: "HAS_FOCUS",
+					},
+					{
+						UUID:  "c28fa0b4-4245-11e8-842f-0ed5f89f718b",
+						Label: "HAS_FOCUS",
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, _ := test.neoConcept.ТоOntologyNewConcept(test.ontologyRelsCfg)
+			if !cmp.Equal(got, test.expected) {
+				t.Error(cmp.Diff(got, test.expected))
+			}
+		})
+	}
+}
--- ontology/transform_test.go
+++ ontology/transform_test.go
@@ -1,6 +1,7 @@
 package ontology
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -48,5 +49,43 @@ func TestTransformAggregateConceptProperties(t *testing.T) {
 	got := TransformToOldAggregateConcept(newAggregateConcept)
 	if !cmp.Equal(got, expected) {
 		t.Errorf("transforming between old and new model has failed:\n%s", cmp.Diff(got, expected))
+	}
+}
+
+func TestTransformSourceConceptRelationships(t *testing.T) {
+	expected := Concept{
+		ParentUUIDs:                []string{"2ef39c2a-da9c-4263-8209-ebfd490d3101"},
+		BroaderUUIDs:               []string{"f7e3fe2d-7496-4d42-b19f-378094efd263", "b5d7c6b5-db7d-4bce-9d6a-f62195571f92"},
+		RelatedUUIDs:               []string{"f7e3fe2d-7496-4d42-b19f-378094efd263", "b5d7c6b5-db7d-4bce-9d6a-f62195571f92"},
+		SupersededByUUIDs:          []string{"1a96ee7a-a4af-3a56-852c-60420b0b8da6", "b5d7c6b5-db7d-4bce-9d6a-f62195571f92"},
+		ImpliedByUUIDs:             []string{"740c604b-8d97-443e-be70-33de6f1d6e67", "b5d7c6b5-db7d-4bce-9d6a-f62195571f92"},
+		HasFocusUUIDs:              []string{"2e7429bd-7a84-41cb-a619-2c702893e359", "740c604b-8d97-443e-be70-33de6f1d6e67", "c28fa0b4-4245-11e8-842f-0ed5f89f718b"},
+		OrganisationUUID:           "7f40d291-b3cb-47c4-9bce-18413e9350cf",
+		PersonUUID:                 "35946807-0205-4fc1-8516-bb1ae141659b",
+		CountryOfRiskUUID:          "coreb1c1-7ecd-4600-8cbb-c02ba53ced4b",
+		CountryOfIncorporationUUID: "coieb1c1-7ecd-4600-8cbb-c02ba53ced4b",
+		CountryOfOperationsUUID:    "cooeb1c1-7ecd-4600-8cbb-c02ba53ced4b",
+		ParentOrganisation:         "c001ee9c-94c5-11e8-8f42-da24cd01f044",
+	}
+
+	newSourceConcept := TransformToNewSourceConcept(expected)
+	got := TransformToOldSourceConcept(newSourceConcept)
+
+	sort.Strings(expected.ParentUUIDs)
+	sort.Strings(expected.BroaderUUIDs)
+	sort.Strings(expected.RelatedUUIDs)
+	sort.Strings(expected.SupersededByUUIDs)
+	sort.Strings(expected.ImpliedByUUIDs)
+	sort.Strings(expected.HasFocusUUIDs)
+
+	sort.Strings(got.ParentUUIDs)
+	sort.Strings(got.BroaderUUIDs)
+	sort.Strings(got.RelatedUUIDs)
+	sort.Strings(got.SupersededByUUIDs)
+	sort.Strings(got.ImpliedByUUIDs)
+	sort.Strings(got.HasFocusUUIDs)
+
+	if !cmp.Equal(got, expected) {
+		t.Errorf("transforming between old and new source model has failed:\n%s", cmp.Diff(got, expected))
 	}
 }
